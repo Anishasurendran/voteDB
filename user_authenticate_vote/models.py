6e78b29827f427ac144639b7bd14687bdfea71ad
Anishasurendran <anishasurendran1996@gmail.com>
2020-01-18 16:07:48 +0530

    'Tryout'

--- user_authenticate_vote/models.py
+++ user_authenticate_vote/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserDetails(models.Model):
-    adhar_number=models.IntegerField(default=0)
+    adhar_number=models.CharField(max_length=12,default=None)
     name=models.CharField(max_length=10,default=None)
     address=models.CharField(max_length=30,default=None)
     father_name=models.CharField(max_length=10,default=None)
@@ -10,7 +10,7 @@ class UserDetails(models.Model):
     dob=models.DateField(default=None)
     district=models.CharField(max_length=15,default=None)
     taluk=models.CharField(max_length=15,default=None)
-    phone_number=models.IntegerField(default=0)
+    phone_number=models.CharField(max_length=10,default=None)
     photo=models.ImageField(default=None)
 
 
