579508205f092f2c727f602f65add51a519bfe8c
Raison Joseph <raisonjosephau@gmail.com>
2020-01-18 16:19:32 +0530

    Add tempelate

--- onlinevote/settings.py
+++ onlinevote/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
-     'user_authenticate_vote',
-     'candidate_vote',
+    'user_authenticate_vote',
+    'candidate_vote',
+    'public',
+
 
 ]
 
---
+++ public/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from .views import LandingView
+urlpatterns = [
+    url("",
+        LandingView.as_view(),
+        name="dashboard"),
+]
