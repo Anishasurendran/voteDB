93bd754cd681db769de9245dfea806962e0117c1
Anu <sinianu2019@gmail.com>
2020-01-18 12:13:51 +0530

    cand

--- candidate_vote/models.py
+++ candidate_vote/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CandidateDetails(models.Model):
+    candidate_name=models.CharField(max_length=15,default=None)
+    cand_address=models.CharField(max_length=30,default=None)
+    cand_gender=models.CharField(max_length=10,default=None)
+    cand_district=models.CharField(max_length=10,default=None)
+    cand_taluk=models.CharField(max_length=10,default=None)
+    cand_phoneno=models.IntegerField(max_length=10,default=None)
+    
