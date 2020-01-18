9f4e8fe1856c7dbaf184ef4aec9daaf55e48cbb1
Anu <sinianu2019@gmail.com>
2020-01-18 15:21:00 +0530

    modify1

--- candidate_vote/admin.py
+++ candidate_vote/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CandidateDetails,Election,Voting,Electioninfo
+from .models import CandidateDetails,Election
 
 @admin.register(CandidateDetails)
 class CandidateDetailsAdmin(admin.ModelAdmin):
     list_display=('candidate_name','candidate_district','candidate_phoneno',)
     ordering=('candidate_name','candidate_phoneno',)
     search_fields=('candidate_name',)
+
 @admin.register(Election)
-class Election(admin.ModelAdmin):
+class ElectionAdmin(admin.ModelAdmin):
     list_display=('election_name','election_sdate','election_edate',)
-    ordering=('election_name','election_sdate','election_eadte',)
-    search_fields=('election_name')
+    ordering=('election_name','election_sdate','election_edate',)
+    search_fields=('election_name',)
