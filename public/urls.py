af555edc579f60f037938c51d55205bf614b1dd9
Raison Joseph <raisonjosephau@gmail.com>
2020-01-22 21:06:37 +0530

    Add argon with register

--- public/urls.py
+++ public/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 
-from .views import LandingView
+from .views import LandingView, RegisterView
 urlpatterns = [
-    url("",
-        LandingView.as_view(),
-        name="dashboard"),
+    path("", LandingView.as_view(), name="dashboard"),
+    path("register/", RegisterView.as_view(), name="join"),
 ]
--- public/views.py
+++ public/views.py
@@ -4,4 +4,7 @@ from django.views.generic import TemplateView
 
 
 class LandingView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'landing.html'
+
+class RegisterView(TemplateView):
+    template_name= 'register.html'
