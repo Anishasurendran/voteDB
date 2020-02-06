19b75eeafc206cfd6a069646fec662fa968fd4ed
Raison Joseph <raisonjosephau@gmail.com>
2020-02-07 01:49:43 +0530

    Forms

--- public/urls.py
+++ public/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import LandingView, RegisterView
+from .views import LandingView, RegisterView, uploadXML
 urlpatterns = [
     path("", LandingView.as_view(), name="dashboard"),
     path("register/", RegisterView.as_view(), name="join"),
+    path("upload_xml/", uploadXML, name="xml_upload"),
 ]
--- public/views.py
+++ public/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic import TemplateView
+from .forms import UploadXMLForm
 
 
 class LandingView(TemplateView):
@@ -8,3 +9,15 @@ class LandingView(TemplateView):
 
 class RegisterView(TemplateView):
     template_name= 'register.html'
+
+def uploadXML(request):
+
+    if request.method == "POST":
+        UploadForm = UploadXMLForm(files=request.FILES)
+        print(request.FILES)
+
+        if UploadForm.is_valid():
+            print(UploadForm.cleaned_data['xml_file'])
+        else:
+            print(UploadForm.errors)
+    return redirect('/register/')
